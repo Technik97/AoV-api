15f23cfd278ad0395eeab7d718b909c659b49ef1
Technik97 <aniket.97.mane@gmail.com>
2020-11-11 19:31:48 +0530

    Added functionality to add user to the context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const { Token } = require('graphql');
 
 const DB_HOST = process.env.DB_HOST;
- 
+
+//get the user info from a JWT
+const getUser = token => {
+    if(token){
+        try {
+            //return the user information from the token
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            //if there is a problem with the token, throw an error
+            throw new Error('Session invalid');
+        }
+    }
+} 
 
 const app = express();
 
@@ -20,9 +34,17 @@ db.connect(DB_HOST);
 const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
-    context: () => {
-        //Add the db models to the context
-        return { models };
+    context: ({ req }) => {
+        //get the user token from the header
+        const token = req.headers.authorization;
+
+        //try to retrieve a user to the console
+        const user = getUser(token);
+
+        console.log(user);
+
+        //Add the db models and the user to the context
+        return { models, user };
     }
 });
 
